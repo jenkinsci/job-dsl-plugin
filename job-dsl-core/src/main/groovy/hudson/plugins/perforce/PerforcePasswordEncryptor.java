2d5529ae58419ba36d3029793ad1a565ccfc5bfe
Justin Ryan <jryan@netflix.com>
2012-11-15 22:07:00 -0800

    Introduce onejar so that script can be run alone

---
+++ job-dsl-core/src/main/groovy/hudson/plugins/perforce/PerforcePasswordEncryptor.java
@@ -0,0 +1,11 @@
+package hudson.plugins.perforce;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jryan
+ * Date: 11/15/12
+ * Time: 1:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PerforcePasswordEncryptor {
+}
