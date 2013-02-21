2cdbb94253d3e6bf56c3b49fdbea9faab9021911
Daniel Spilker <daniel.spilker@coremedia.com>
2013-02-21 16:46:53 +0100

    removed debug output

--- job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/DslSampleTest.groovy
+++ job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/DslSampleTest.groovy
@@ -26,7 +26,6 @@ class DslSampleTest extends Specification {
         // TODO Review actual results
         println(firstJob)
         def mavenJob = jm.savedConfigs['PROJ-maven']
-        println mavenJob
         assertXMLEqual '<?xml version="1.0" encoding="UTF-8"?>' + mavenXml, mavenJob
     }
 
