d096de23a71a77288da45011b6e4259ef17544b4
Justin Harringa <justin@harringa.com>
2013-10-03 12:42:00 -0500

    Added ItemDiscover, ItemCancel, and ScmTag permissions to the enum
    These additional permissions should be useful for other standard permissions.

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/Permissions.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/Permissions.groovy
@@ -6,10 +6,13 @@ enum Permissions {
     ItemDelete('hudson.model.Item.Delete'),
     ItemBuild('hudson.model.Item.Build'),
     ItemRead('hudson.model.Item.Read'),
+    ItemDiscover('hudson.model.Item.Discover'),
+    ItemCancel('hudson.model.Item.Cancel'),
     ItemRelease('hudson.model.Item.Release'),
     ItemExtendedRead('hudson.model.Item.ExtendedRead'),
     RunDelete('hudson.model.Run.Delete'),
-    RunUpdate('hudson.model.Run.Update')
+    RunUpdate('hudson.model.Run.Update'),
+    ScmTag('hudson.scm.SCM.Tag')
 
     final String longForm
     Permissions(String longForm) {
