791deeaa50c118934a3dd1f05d96942bffe96cf0
Jens Hausherr <jens.hausherr@xing.com>
2014-01-08 09:53:41 +0100

    Cleaned up unused imports and whitespace

--- job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/MonitorTemplateJobs.java
+++ job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/MonitorTemplateJobs.java
@@ -4,7 +4,6 @@ package javaposse.jobdsl.plugin;
 import com.google.common.base.Function;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Multimaps;
 import hudson.Extension;
 import hudson.Util;
 import hudson.XmlFile;
@@ -15,7 +14,6 @@ import hudson.model.Saveable;
 import hudson.model.listeners.SaveableListener;
 import jenkins.YesNoMaybe;
 import jenkins.model.Jenkins;
-import org.apache.commons.collections.MultiMap;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -48,25 +46,21 @@ public class MonitorTemplateJobs extends SaveableListener {
         String possibleTemplateName = project.getName();
 
         DescriptorImpl descriptor = Jenkins.getInstance().getDescriptorByType(DescriptorImpl.class);
-
         if (descriptor == null) {
             LOGGER.warning("Unable to get DescriptorImpl");
         }
 
         Multimap<String,SeedReference> templateJobMap = (descriptor != null? descriptor.getTemplateJobMap(): null);
-
         if (templateJobMap == null) {
             LOGGER.warning("Descriptor returned no template job map.");
         }
 
         Collection<SeedReference> seedJobReferences = templateJobMap != null? templateJobMap.get(possibleTemplateName): Collections.<SeedReference>emptyList();
-
         if (seedJobReferences.isEmpty()) {
             return;
         }
 
         final String digest;
-
         try {
             digest = Util.getDigestOf(new FileInputStream(project.getConfigFile().getFile()));
         } catch (IOException e) {
