7a425497ad1fcd118fe3c9ff8fd7b5de2bb2f52a
Behrooz Nobakht <behrooz.nobakht@gmail.com>
2014-01-28 08:22:49 +0100

    JENKINS-21408 Use 4-space formatting

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/publisher/RobotFrameworkContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/publisher/RobotFrameworkContext.groovy
@@ -15,44 +15,44 @@ import javaposse.jobdsl.dsl.helpers.Context
  */
 class RobotFrameworkContext implements Context {
 
-	static String DEFAULT_OUTPUT_PATH = "target/robotframework-reports"
-	static String DEFAULT_REPORT_FILE_NAME = "report.html"
-	static String DEFAULT_OUTPUT_FILE_NAME = "output.xml"
-	static String DEFAULT_LOG_FILE_NAME = "log.html"
+    static String DEFAULT_OUTPUT_PATH = "target/robotframework-reports"
+    static String DEFAULT_REPORT_FILE_NAME = "report.html"
+    static String DEFAULT_OUTPUT_FILE_NAME = "output.xml"
+    static String DEFAULT_LOG_FILE_NAME = "log.html"
 
-	Double passThreshold = 100.0
-	Double unstableThreshold = 0.0
-	Boolean onlyCritical = false
-	String outputPath = DEFAULT_OUTPUT_PATH
-	String reportFileName = DEFAULT_REPORT_FILE_NAME
-	String logFileName = DEFAULT_LOG_FILE_NAME
-	String outputFileName = DEFAULT_OUTPUT_FILE_NAME
-	
-	void passThreshold(Double passThreshold) {
-		this.passThreshold = passThreshold ?: this.passThreshold
-	}
-	
-	void unstableThreshold(Double unstableThreshold) {
-		this.unstableThreshold = unstableThreshold ?: this.unstableThreshold
-	}
-	
-	void onlyCritical(Boolean onlyCritical) {
-		this.onlyCritical = onlyCritical ?: this.onlyCritical
-	}
-	
-	void outputPath(String outputPath) {
-		this.outputPath = outputPath ?: this.outputPath
-	}
-	
-	void reportFileName(String reportFileName) {
-		this.reportFileName = reportFileName ?: this.reportFileName
-	}
-	
-	void logFileName(String logFileName) {
-		this.logFileName = logFileName ?: this.logFileName
-	}
-	
-	void outputFileName(String outputFileName) {
-		this.outputFileName = outputFileName ?: this.outputFileName
-	}
+    Double passThreshold = 100.0
+    Double unstableThreshold = 0.0
+    Boolean onlyCritical = false
+    String outputPath = DEFAULT_OUTPUT_PATH
+    String reportFileName = DEFAULT_REPORT_FILE_NAME
+    String logFileName = DEFAULT_LOG_FILE_NAME
+    String outputFileName = DEFAULT_OUTPUT_FILE_NAME
+
+    void passThreshold(Double passThreshold) {
+        this.passThreshold = passThreshold ?: this.passThreshold
+    }
+
+    void unstableThreshold(Double unstableThreshold) {
+        this.unstableThreshold = unstableThreshold ?: this.unstableThreshold
+    }
+
+    void onlyCritical(Boolean onlyCritical) {
+        this.onlyCritical = onlyCritical ?: this.onlyCritical
+    }
+
+    void outputPath(String outputPath) {
+        this.outputPath = outputPath ?: this.outputPath
+    }
+
+    void reportFileName(String reportFileName) {
+        this.reportFileName = reportFileName ?: this.reportFileName
+    }
+
+    void logFileName(String logFileName) {
+        this.logFileName = logFileName ?: this.logFileName
+    }
+
+    void outputFileName(String outputFileName) {
+        this.outputFileName = outputFileName ?: this.outputFileName
+    }
 }
