20c0e4642299e942fd9da941a5c1273e722f6fe3
Behrooz Nobakht <behrooz.nobakht@gmail.com>
2014-01-29 05:20:15 +0100

    JENKINS-21408 Apply 4-space formatting

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/publisher/PublisherContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/publisher/PublisherContext.groovy
@@ -892,43 +892,43 @@ class PublisherContext implements Context {
             }
         }
     }
-	
-	/**
-	 * Configures Jenkins job to publish Robot Framework reports.
-	 * By default the following values are applied. If an instance of a 
-	 * closure is provided, the values from the closure will take effect.
-	 * 
-	 * {@code 
-	 *   <publishers>
-	 *		<hudson.plugins.robot.RobotPublisher plugin="robot@1.3.2">
-	 * 			<outputPath>target/robotframework-reports</outputPath>
-	 *			<passThreshold>100.0</passThreshold>
-	 *			<unstableThreshold>0.0</unstableThreshold>
-	 * 			<onlyCritical>false</onlyCritical>
-	 * 			<reportFileName>report.html</reportFileName>
-	 * 			<logFileName>log.html</logFileName>
-	 * 			<outputFileName>output.xml</outputFileName>
-	 * 		</hudson.plugins.robot.RobotPublisher>
-	 *	</publishers>
-	 *}
-	 * @see https://wiki.jenkins-ci.org/display/JENKINS/Robot+Framework+Plugin
-	 */
-	def publishRobotFrameworkReports(Closure robotClosure = null) {
-		
-		RobotFrameworkContext ctx = new RobotFrameworkContext()
-		AbstractContextHelper.executeInContext(robotClosure, ctx)
-		
-		def nodeBuilder = NodeBuilder.newInstance()
-		Node robotNode = nodeBuilder.'hudson.plugins.robot.RobotPublisher' {
-			passThreshold ctx.passThreshold
-			unstableThreshold ctx.unstableThreshold
-			outputPath ctx.outputPath
-			onlyCritical ctx.onlyCritical
-			reportFileName ctx.reportFileName
-			logFileName ctx.logFileName
-			outputFileName ctx.outputFileName
-		}
-		
-		publisherNodes << robotNode
-	}
+    
+    /**
+     * Configures Jenkins job to publish Robot Framework reports.
+     * By default the following values are applied. If an instance of a
+     * closure is provided, the values from the closure will take effect.
+     *
+     * {@code
+     *   <publishers>
+     *      <hudson.plugins.robot.RobotPublisher plugin="robot@1.3.2">
+     *          <outputPath>target/robotframework-reports</outputPath>
+     *          <passThreshold>100.0</passThreshold>
+     *          <unstableThreshold>0.0</unstableThreshold>
+     *          <onlyCritical>false</onlyCritical>
+     *          <reportFileName>report.html</reportFileName>
+     *          <logFileName>log.html</logFileName>
+     *          <outputFileName>output.xml</outputFileName>
+     *      </hudson.plugins.robot.RobotPublisher>
+     *  </publishers>
+     *}
+     * @see https://wiki.jenkins-ci.org/display/JENKINS/Robot+Framework+Plugin
+     */
+    def publishRobotFrameworkReports(Closure robotClosure = null) {
+
+        RobotFrameworkContext ctx = new RobotFrameworkContext()
+        AbstractContextHelper.executeInContext(robotClosure, ctx)
+
+        def nodeBuilder = NodeBuilder.newInstance()
+        Node robotNode = nodeBuilder.'hudson.plugins.robot.RobotPublisher' {
+            passThreshold ctx.passThreshold
+            unstableThreshold ctx.unstableThreshold
+            outputPath ctx.outputPath
+            onlyCritical ctx.onlyCritical
+            reportFileName ctx.reportFileName
+            logFileName ctx.logFileName
+            outputFileName ctx.outputFileName
+        }
+
+        publisherNodes << robotNode
+    }
 }
