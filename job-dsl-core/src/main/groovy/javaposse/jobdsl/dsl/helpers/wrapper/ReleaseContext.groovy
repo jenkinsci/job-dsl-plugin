14dd6c673867fc6415e1f4b41e8c047bc0ab723f
Thomas Bosch <tbosch@gmx.de>
2014-01-31 23:23:53 +0100

    Jenkins Release Plugin
    releaseVersionTemplate made nullable
    
    reformat with emacs
    
    changed formatting
    
    use primitive boolean and append CRLF at the end of file
    
    add test and comments
    
    fixed review tasks
    
    more params
    
    use addAll instead of <<
    
    fixed hints
    
    remove comments

---
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/wrapper/ReleaseContext.groovy
@@ -0,0 +1,64 @@
+package javaposse.jobdsl.dsl.helpers.wrapper
+
+import javaposse.jobdsl.dsl.helpers.AbstractContextHelper
+import javaposse.jobdsl.dsl.helpers.BuildParametersContext
+import javaposse.jobdsl.dsl.helpers.Context
+import javaposse.jobdsl.dsl.helpers.step.AbstractStepContext
+
+class ReleaseContext implements Context {
+    String releaseVersionTemplate
+    boolean doNotKeepLog
+    boolean overrideBuildParameters
+    List<Node> params = []
+    List<Node> preBuildSteps = []
+    List<Node> postSuccessfulBuildSteps = []
+    List<Node> postBuildSteps = []
+    List<Node> postFailedBuildSteps = []
+    Closure configureBlock
+
+    def preBuildSteps(Closure closure) {
+        def stepContext = new AbstractStepContext()
+        AbstractContextHelper.executeInContext(closure, stepContext)
+        preBuildSteps.addAll(stepContext.stepNodes)
+    }
+
+    def postSuccessfulBuildSteps(Closure closure) {
+        def stepContext = new AbstractStepContext()
+        AbstractContextHelper.executeInContext(closure, stepContext)
+        postSuccessfulBuildSteps.addAll(stepContext.stepNodes)
+    }
+
+    def postBuildSteps(Closure closure) {
+        def stepContext = new AbstractStepContext()
+        AbstractContextHelper.executeInContext(closure, stepContext)
+        postBuildSteps.addAll(stepContext.stepNodes)
+    }
+
+    def postFailedBuildSteps(Closure closure) {
+        def stepContext = new AbstractStepContext()
+        AbstractContextHelper.executeInContext(closure, stepContext)
+        postFailedBuildSteps.addAll(stepContext.stepNodes)
+    }
+
+    def releaseVersionTemplate(String releaseVersionTemplate) {
+        this.releaseVersionTemplate = releaseVersionTemplate
+    }
+
+    def doNotKeepLog(boolean doNotKeepLog) {
+        this.doNotKeepLog = doNotKeepLog
+    }
+
+    def overrideBuildParameters(boolean overrideBuildParameters) {
+        this.overrideBuildParameters = overrideBuildParameters
+    }
+
+    def configure(Closure closure) {
+        this.configureBlock = closure
+    }
+
+    def parameters(Closure parametersClosure) {
+        BuildParametersContext parametersContext = new BuildParametersContext()
+        AbstractContextHelper.executeInContext(parametersClosure, parametersContext)
+        params.addAll(parametersContext.buildParameterNodes.values())
+    }
+}
