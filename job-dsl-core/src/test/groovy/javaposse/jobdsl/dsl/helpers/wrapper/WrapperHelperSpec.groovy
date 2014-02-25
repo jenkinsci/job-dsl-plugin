e53fb46233945d94b81f81991d21473421c8fdc1
Matthias Balke <matthias.balke@googlemail.com>
2014-02-25 16:35:00 +0100

    [JENKINS-21947] added DSL support for m2release plugin

--- job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/helpers/wrapper/WrapperHelperSpec.groovy
+++ job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/helpers/wrapper/WrapperHelperSpec.groovy
@@ -737,4 +737,62 @@ class WrapperHelperSpec extends Specification {
             ids[0].'org.jvnet.hudson.plugins.exclusion.DefaultIdType'[2].name[0].value() == 'third'
         }
     }
+
+    def 'configure m2release plugin with least args'() {
+        when:
+        context = new WrapperContext(JobType.Maven, mockJobManagement)
+        context.mavenRelease()
+
+        then:
+        context.wrapperNodes.size() == 1
+        def m2releaseNode = context.wrapperNodes[0]
+
+        m2releaseNode.scmUserEnvVar[0].value() == ''
+        m2releaseNode.scmPasswordEnvVar[0].value() == ''
+        m2releaseNode.releaseEnvVar[0].value() == 'IS_M2RELEASEBUILD'
+        m2releaseNode.releaseGoals[0].value() == '-Dresume=false release:prepare release:perform'
+        m2releaseNode.dryRunGoals[0].value() == '-Dresume=false -DdryRun=true release:prepare'
+        m2releaseNode.selectCustomScmCommentPrefix[0].value() == false
+        m2releaseNode.selectAppendHudsonUsername[0].value() == false
+        m2releaseNode.selectScmCredentials[0].value() == false
+        m2releaseNode.numberOfReleaseBuildsToKeep[0].value() == 1
+    }
+
+    def 'configure m2release plugin with all args'() {
+        when:
+        context = new WrapperContext(JobType.Maven, mockJobManagement)
+        context.mavenRelease() {
+            scmUserEnvVar 'MY_USER_ENV'
+            scmPasswordEnvVar 'MY_PASSWORD_ENV'
+            releaseEnvVar 'RELEASE_ENV'
+            releaseGoals 'release:prepare release:perform'
+            dryRunGoals '-DdryRun=true release:prepare'
+            selectCustomScmCommentPrefix()
+            selectAppendHudsonUsername()
+            selectScmCredentials()
+            numberOfReleaseBuildsToKeep 10
+        }
+
+        then:
+        context.wrapperNodes.size() == 1
+        def m2releaseNode = context.wrapperNodes[0]
+
+        m2releaseNode.scmUserEnvVar[0].value() == 'MY_USER_ENV'
+        m2releaseNode.scmPasswordEnvVar[0].value() == 'MY_PASSWORD_ENV'
+        m2releaseNode.releaseEnvVar[0].value() == 'RELEASE_ENV'
+        m2releaseNode.releaseGoals[0].value() == 'release:prepare release:perform'
+        m2releaseNode.dryRunGoals[0].value() == '-DdryRun=true release:prepare'
+        m2releaseNode.selectCustomScmCommentPrefix[0].value() == true
+        m2releaseNode.selectAppendHudsonUsername[0].value() == true
+        m2releaseNode.selectScmCredentials[0].value() == true
+        m2releaseNode.numberOfReleaseBuildsToKeep[0].value() == 10
+    }
+
+    def 'configure m2release plugin with FreeForm job should fail'() {
+        when:
+        context.mavenRelease()
+
+        then:
+        thrown IllegalStateException
+    }
 }
