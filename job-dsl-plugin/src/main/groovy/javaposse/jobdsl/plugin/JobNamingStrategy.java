c9e0f328def9b6f171a1637eefa5e94bb15c6b44
Wilco Greven <wilco.greven@ibridge.nl>
2014-03-18 17:29:02 +0100

    Add some documentation.

--- job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/JobNamingStrategy.java
+++ job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/JobNamingStrategy.java
@@ -5,7 +5,16 @@ import hudson.model.Item;
 import hudson.model.ItemGroup;
 import jenkins.model.Jenkins;
 
+/**
+ * A JobNamingStrategy encapsulates where a seed job will look for existing jobs
+ * and where it will create new jobs. This matters when you use relative names in
+ * a job dsl script and the seed job is in a folder.
+ */
 public enum JobNamingStrategy {
+    /**
+     * A naming strategy to provide backwards compatibility for jobs created with
+     * plugin versions prior to 1.22.
+     */
     JENKINS_ROOT("Jenkins Root") {
         @Override
         public Item getItem(String pathName, AbstractProject<?,?> seedJob) {
@@ -17,6 +26,10 @@ public enum JobNamingStrategy {
             return Jenkins.getInstance();
         }
     },
+    /**
+     * Using this naming strategy jobs with relative path names are created relative
+     * to the seed job's parent folder.
+     */
     SEED_JOB("Seed Job") {
         @Override
         public Item getItem(String pathName, AbstractProject<?,?> seedJob) {
