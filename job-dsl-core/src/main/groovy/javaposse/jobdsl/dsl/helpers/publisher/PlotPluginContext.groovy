b9ae9abe70829faab8493d2d3dacccaec658a37c
Rob Langley <rob.langley@bromium.com>
2014-06-19 19:09:07 +0100

    adding support for other chart types

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/publisher/PlotPluginContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/publisher/PlotPluginContext.groovy
@@ -10,6 +10,38 @@ class PlotPluginContext implements Context {
         processPlotClosure('area', dataStore, group, plotPluginPlotClosure)
     }
 
+    void bar(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('bar', dataStore, group, plotPluginPlotClosure)
+    }
+
+    void bar3d(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('bar3d', dataStore, group, plotPluginPlotClosure)
+    }
+
+    void line(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('line', dataStore, group, plotPluginPlotClosure)
+    }
+
+    void line3d(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('line3d', dataStore, group, plotPluginPlotClosure)
+    }
+
+    void stackedArea(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('stackedArea', dataStore, group, plotPluginPlotClosure)
+    }
+
+    void stackedbar(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('stackedbar', dataStore, group, plotPluginPlotClosure)
+    }
+
+    void stackedbar3d(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('stackedbar3d', dataStore, group, plotPluginPlotClosure)
+    }
+
+    void waterfall(String dataStore, String group, Closure plotPluginPlotClosure) {
+        processPlotClosure('waterfall', dataStore, group, plotPluginPlotClosure)
+    }
+
     private void processPlotClosure(String style, String dataStore, String group, Closure plotPluginPlotClosure) {
         PlotPluginPlotContext plotPluginPlotContext = new PlotPluginPlotContext(style, dataStore, group)
         AbstractContextHelper.executeInContext(plotPluginPlotClosure, plotPluginPlotContext)
