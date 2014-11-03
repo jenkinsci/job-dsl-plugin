fa77dbecaad2afe85afc2a22e6dd0855e6c32fde
Malte Hübner <malte.huebner@jimdo.com>
2014-11-03 17:17:02 +0100

    [JENKINS-24774] Implemented new ViewType for BuildMonitorPlugin.

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/ViewType.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/ViewType.groovy
@@ -5,7 +5,8 @@ enum ViewType {
     SectionedView(javaposse.jobdsl.dsl.views.SectionedView),
     NestedView(javaposse.jobdsl.dsl.views.NestedView),
     DeliveryPipelineView(javaposse.jobdsl.dsl.views.DeliveryPipelineView),
-    BuildPipelineView(javaposse.jobdsl.dsl.views.BuildPipelineView)
+    BuildPipelineView(javaposse.jobdsl.dsl.views.BuildPipelineView),
+    BuildMonitorView(javaposse.jobdsl.dsl.views.BuildMonitorView)
 
     final Class<? extends View> viewClass
 
