d03f4d136a960fe9ebe787a12e4221957c9b7f42
Daniel Spilker <mail@daniel-spilker.com>
2015-07-03 21:20:48 +0200

    fixed order of imports

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/JobManagement.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/JobManagement.groovy
@@ -1,7 +1,7 @@
 package javaposse.jobdsl.dsl
 
-import javaposse.jobdsl.dsl.helpers.ExtensibleContext
 import hudson.util.VersionNumber
+import javaposse.jobdsl.dsl.helpers.ExtensibleContext
 
 /**
  * Interface to manage jobs, which the DSL needs to do.
