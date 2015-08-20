4dd8ee76a8490cf8204ddd5834e91ad3b508bdbb
Daniel Spilker <mail@daniel-spilker.com>
2015-08-20 12:01:19 +0200

    do not use star imports

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/RequiresPlugin.java
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/RequiresPlugin.java
@@ -1,6 +1,10 @@
 package javaposse.jobdsl.dsl;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 // this has to be written in Java because of https://jira.codehaus.org/browse/GROOVY-6019
 
