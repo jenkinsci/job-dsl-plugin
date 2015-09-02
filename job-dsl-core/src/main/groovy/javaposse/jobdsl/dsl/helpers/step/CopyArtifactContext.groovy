15c178464935f6a5efa4b8e62547b0db93e0efbb
Daniel Spilker <mail@daniel-spilker.com>
2015-09-02 21:27:59 +0200

    fixed problem with fully qualified imports of javaposse.jobdsl.dsl.DslContext

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/step/CopyArtifactContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/step/CopyArtifactContext.groovy
@@ -2,6 +2,7 @@ package javaposse.jobdsl.dsl.helpers.step
 
 import javaposse.jobdsl.dsl.AbstractContext
 import javaposse.jobdsl.dsl.ContextHelper
+import javaposse.jobdsl.dsl.DslContext
 import javaposse.jobdsl.dsl.JobManagement
 import javaposse.jobdsl.dsl.RequiresPlugin
 
@@ -66,7 +67,7 @@ class CopyArtifactContext extends AbstractContext {
     /**
      * Selects the build to copy artifacts from.
      */
-    void buildSelector(@javaposse.jobdsl.dsl.DslContext(CopyArtifactSelectorContext) Closure selectorClosure) {
+    void buildSelector(@DslContext(CopyArtifactSelectorContext) Closure selectorClosure) {
         ContextHelper.executeInContext(selectorClosure, selectorContext)
     }
 }
--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/step/PhaseJobContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/step/PhaseJobContext.groovy
@@ -2,6 +2,7 @@ package javaposse.jobdsl.dsl.helpers.step
 
 import javaposse.jobdsl.dsl.AbstractContext
 import javaposse.jobdsl.dsl.ContextHelper
+import javaposse.jobdsl.dsl.DslContext
 import javaposse.jobdsl.dsl.JobManagement
 import javaposse.jobdsl.dsl.Preconditions
 import javaposse.jobdsl.dsl.RequiresPlugin
@@ -52,7 +53,7 @@ class PhaseJobContext extends AbstractContext {
      * Adds parameter values for the job.
      */
     @RequiresPlugin(id = 'parameterized-trigger')
-    void parameters(@javaposse.jobdsl.dsl.DslContext(DownstreamTriggerParameterContext) Closure closure) {
+    void parameters(@DslContext(DownstreamTriggerParameterContext) Closure closure) {
         jobManagement.logPluginDeprecationWarning('parameterized-trigger', '2.25')
 
         ContextHelper.executeInContext(closure, paramTrigger)
