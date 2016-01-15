aa5b0f7ef439458f75bff2f8c79189ce9919cbaf
Dmitry Polyanitsa <d.polyanitsa@criteo.com>
2016-01-15 11:29:25 +0100

    Add an example for the shell step with closure
    Change-Id: I723b34701938f8b5f172e6cdcda3a592b8ff18bf

--- job-dsl-core/src/main/docs/examples/javaposse/jobdsl/dsl/helpers/step/StepContext/shell.groovy
+++ job-dsl-core/src/main/docs/examples/javaposse/jobdsl/dsl/helpers/step/StepContext/shell.groovy
@@ -2,5 +2,9 @@ job('example') {
     steps {
         shell('echo Hello World!')
         shell(readFileFromWorkspace('build.sh'))
+        shell {
+            block('MESSAGE="Hello, World!"')
+            block('echo $MESSAGE')
+        }
     }
 }
