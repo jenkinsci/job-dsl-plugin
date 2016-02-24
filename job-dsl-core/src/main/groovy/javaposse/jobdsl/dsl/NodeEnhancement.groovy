93e34508ec44c7583d7872e09a673fdc75471f27
Daniel Spilker <mail@daniel-spilker.com>
2016-02-24 21:08:29 +0100

    clone nodes before appending
    [FIXES JENKINS-32941]

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/NodeEnhancement.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/NodeEnhancement.groovy
@@ -13,27 +13,28 @@ class NodeEnhancement {
     private static final Logger LOGGER = Logger.getLogger(NodeEnhancement.name)
 
     Node div(Node orphan) {
-        LOGGER.fine("Looking for child node ${orphan}")
-        String childName = orphan.name()
+        Node clonedOrphan = cloneNode(orphan)
+        LOGGER.fine("Looking for child node ${clonedOrphan}")
+        String childName = clonedOrphan.name()
         List children = this.children().findAll { child ->
             child instanceof Node && child.name() == childName &&
-                    child.attributes().entrySet().containsAll(orphan.attributes().entrySet())
+                    child.attributes().entrySet().containsAll(clonedOrphan.attributes().entrySet())
         }
         if (children.size() == 0) {
             LOGGER.fine("Creating node for ${childName}")
             // Create node using just name
-            this.append(orphan)
-            orphan
+            this.append(clonedOrphan)
+            clonedOrphan
         } else {
             // Return first childName, that's the contract for div
             LOGGER.fine("Using first found childName for ${childName}")
             Node found = children[0] as Node
 
             // Copy over value and attribute from orphan if it has one.
-            if (orphan.value() != null) {
-                found.setValue(orphan.value())
+            if (clonedOrphan.value() != null) {
+                found.setValue(clonedOrphan.value())
             }
-            orphan.attributes().each { k, v ->
+            clonedOrphan.attributes().each { k, v ->
                 found.attributes().put(k, v)
             }
 
@@ -76,7 +77,7 @@ class NodeEnhancement {
 
     Node leftShift(Node child) {
         LOGGER.fine("Appending node ${child} to ${this}")
-        this.append(child)
+        this.append(cloneNode(child))
         this
     }
 
@@ -86,5 +87,39 @@ class NodeEnhancement {
         List<Node> newChildren = buildChildren(configureBlock)
         newChildren.each { this.append(it) }
         this
+    }
+
+    /**
+     * Creates a new Node with the same name, no parent, shallow cloned attributes
+     * and if the value is a NodeList, a (deep) clone of those nodes.
+     *
+     * @return the clone
+     */
+    // can be replaced by Node#clone() when using a Groovy release that contains fixes for GROOVY-5682 and GROOVY-7044
+    private static Node cloneNode(Node node) {
+        Object newValue = node.value()
+        if (newValue instanceof List) {
+            newValue = cloneNodeList((List) newValue)
+        }
+        new Node(null, node.name(), new HashMap(node.attributes()), newValue)
+    }
+
+    /**
+     * Creates a new NodeList containing the same elements as the
+     * original (but cloned in the case of Nodes).
+     *
+     * @return the clone
+     */
+    private static List cloneNodeList(List nodeList) {
+        List result = nodeList instanceof NodeList ? new NodeList(nodeList.size()) : new ArrayList(nodeList.size())
+        for (int i = 0; i < nodeList.size(); i++) {
+            Object next = nodeList[i]
+            if (next instanceof Node) {
+                result << cloneNode(((Node) next))
+            } else {
+                result << next
+            }
+        }
+        result
     }
 }
