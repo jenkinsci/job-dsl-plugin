3f538482021e1a195419a69b593664c8903259e3
Daniel Spilker <mail@daniel-spilker.com>
2016-04-18 23:30:27 +0200

    added embedded API Viewer

---
+++ job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/JobDslPlugin.groovy
@@ -0,0 +1,42 @@
+package javaposse.jobdsl.plugin
+
+import hudson.Plugin
+import jenkins.model.Jenkins
+import net.sf.json.JSONObject
+import org.kohsuke.stapler.StaplerRequest
+import org.kohsuke.stapler.StaplerResponse
+
+import javax.servlet.ServletException
+
+import static hudson.model.UpdateCenter.ID_DEFAULT
+
+class JobDslPlugin extends Plugin {
+    private JSONObject api
+    private String version
+
+    @Override
+    void postInitialize() throws Exception {
+        version = wrapper.version.split()[0]
+
+        File baseDir = new File(wrapper.baseResourceURL.toURI())
+        api = JSONObject.fromObject(new File(baseDir, "api-viewer/build/data/dsl-${version}.json").text)
+        api.element('embedded', true)
+    }
+
+    @Override
+    void doDynamic(StaplerRequest request, StaplerResponse response) throws IOException, ServletException {
+        String path = request.restOfPath
+        if (path == '/api-viewer') {
+            response.sendRedirect("${request.requestURI}/index.html")
+        } else if (path == '/api-viewer/build/data/update-center.jsonp') {
+            JSONObject data = Jenkins.instance.updateCenter.getById(ID_DEFAULT).JSONObject
+            response.contentType = 'application/javascript'
+            response.writer.print("updateCenter.post(${data.toString()})")
+        } else if (path == "/api-viewer/build/data/dsl-${version}.json") {
+            response.contentType = 'application/json'
+            response.writer.print(api.toString())
+        } else {
+            super.doDynamic(request, response)
+        }
+    }
+}
