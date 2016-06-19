414e97d9857f3401596f2eb2236cfc394422a1ad
ddd-workshop <myrocket@o2.pl>
2016-06-19 19:49:48 +0200

    clearer shell step examples

--- job-dsl-core/src/main/docs/examples/javaposse/jobdsl/dsl/helpers/step/StepContext/shell.groovy
+++ job-dsl-core/src/main/docs/examples/javaposse/jobdsl/dsl/helpers/step/StepContext/shell.groovy
@@ -1,6 +1,13 @@
-job('example') {
+// execute echo command
+job('example-1') {
     steps {
         shell('echo Hello World!')
+    }
+}
+
+// read file from workspace
+job('example-2') {
+    steps {
         shell(readFileFromWorkspace('build.sh'))
     }
 }
