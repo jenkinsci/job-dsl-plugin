f6cfdd9cc5e99765a37fe1628139164b086960e7
Mike Kobit <mkobit@gmail.com>
2017-01-04 09:08:56 -0600

    Fix indentation in `OrganizationFolderTriggerContextSpec`

--- job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/helpers/workflow/OrganizationFolderTriggerContextSpec.groovy
+++ job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/helpers/workflow/OrganizationFolderTriggerContextSpec.groovy
@@ -6,31 +6,31 @@ import spock.lang.Specification
 
 class OrganizationFolderTriggerContextSpec extends Specification {
 
-  private final JobManagement jobManagement = Mock(JobManagement)
-  private final Item item = Mock(Item)
-  private final OrganizationFolderTriggerContext context = new OrganizationFolderTriggerContext(jobManagement, item)
+    private final JobManagement jobManagement = Mock(JobManagement)
+    private final Item item = Mock(Item)
+    private final OrganizationFolderTriggerContext context = new OrganizationFolderTriggerContext(jobManagement, item)
 
-  def "no triggers configured by default"() {
-    expect:
-    context.triggerNodes.empty
-  }
-
-  def "can configure a PeriodicFolderTrigger"() {
-    given:
-    OrganizationFolderTriggerContext.PeriodicFolderTrigger expectedTrigger =
-      OrganizationFolderTriggerContext.PeriodicFolderTrigger.EIGHT_HOURS
-
-    when:
-    context.periodicIfNotOtherwiseTriggered(expectedTrigger)
-
-    then:
-    context.triggerNodes.size() == 1
-    with(context.triggerNodes[0]) {
-      name() == 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger'
-      spec.size() == 1
-      spec[0].value() == expectedTrigger.cron
-      interval.size() == 1
-      interval[0].value() == expectedTrigger.interval
+    def "no triggers configured by default"() {
+        expect:
+        context.triggerNodes.empty
     }
-  }
+
+    def "can configure a PeriodicFolderTrigger"() {
+        given:
+        OrganizationFolderTriggerContext.PeriodicFolderTrigger expectedTrigger =
+            OrganizationFolderTriggerContext.PeriodicFolderTrigger.EIGHT_HOURS
+
+        when:
+        context.periodicIfNotOtherwiseTriggered(expectedTrigger)
+
+        then:
+        context.triggerNodes.size() == 1
+        with(context.triggerNodes[0]) {
+            name() == 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger'
+            spec.size() == 1
+            spec[0].value() == expectedTrigger.cron
+            interval.size() == 1
+            interval[0].value() == expectedTrigger.interval
+        }
+    }
 }
