f853d9d0311069c72dcf81bee76f3f8bf9264ea0
g537597@genmills <sam.oyen@genmills.com>
2017-01-06 17:43:33 -0600

    Added tests to get 100% coverage on VerifyNodeAgainstWhitelistHelper

---
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/VerifyNodeAgainstWhitelistHelper.groovy
@@ -0,0 +1,92 @@
+package javaposse.jobdsl.dsl
+
+import java.util.logging.Logger
+import java.util.logging.Level
+
+/**
+ * Helper class for checking if nodes proceesed are valid according the user specified Whitelist
+ */
+class VerifyNodeAgainstWhitelistHelper {
+    private static final Logger LOGGER = Logger.getLogger(VerifyNodeAgainstWhitelistHelper.name)
+
+    private VerifyNodeAgainstWhitelistHelper() {
+    }
+
+    /**
+     * Verify that configure block is valid according to the whitelist node
+     */
+    static void verifyConfigureBlock(Closure configureBlock, Node whitelistNode) {
+        // todo - should we simply get the whitelistNode from the configureBlock.delegate.jobManagerment, etc
+        Node node = new Node(null, 'project')
+
+        if (configureBlock) {
+            // convert configure block to node form - with parent as root node
+            Closure dup = ((Closure)configureBlock.clone())
+            dup.delegate = new MissingPropertyToStringDelegate(node)
+
+            use(NodeEnhancement) {
+                dup.call(node)
+            }
+
+            // check that all children for the node representing this configure block are valid
+            // we don't need to check root since that's simply 'project'
+            verifyNodeChildren(node, whitelistNode)
+        }
+    }
+
+    /**
+     * Verify that all children of a node do not violate the whitelist by passing in two nodes; the node
+     * whose children need to be verified, and the node representing the whitelist at that same level
+     */
+    static void verifyNodeChildren(Node nodeToVerify, Node whitelistNode) {
+        // we need to check that each nodeToVerify child has a matching element in the whitelist node
+        // at the correct level
+        nodeToVerify.children().each {
+            if (it instanceof Node) {
+                Node childNode = ((Node) it)
+                verifyNode(childNode, whitelistNode)
+            }
+        }
+    }
+
+    /**
+     * Verify that a node does not violate the whitelist by passing in two nodes; the node that
+     * needs to be verified, and the node representing the whitelist at one level higher that the nodeToVerify
+     */
+    static void verifyNode(Node nodeToVerify, Node whitelistNodeParent) {
+        // get whitelist node children names
+        String nodeToVerifyName = nodeToVerify.name().toString()
+        List<String> whitelistNodeChildrenNames = getNodeChildrenNames(whitelistNodeParent)
+        if (whitelistNodeChildrenNames.size() <= 0) {
+            // if no whitelist children, nodeToVerify is valid
+            LOGGER.log(Level.FINE, String.format("No children for whitelist node - " +
+                    "${whitelistNodeParent.name().toString()} - so current jobdsl node - ${nodeToVerifyName} - " +
+                    "and all children are valid, since all parents of current jobdsl node were in whitelist"))
+        }
+        // if whitelist node has children, we check if nodeToVerify is valid
+        else if (whitelistNodeChildrenNames.contains(nodeToVerifyName)) {
+            // since we assume no duplicate names for sibling children in the whitelist node tree (convention of
+            // writing your whitelist xml), we will always take the first one that matches
+            NodeList matchingWhitelistNodeList = ((NodeList)whitelistNodeParent[nodeToVerifyName])
+            Node matchingWhitelistNode = ((Node)matchingWhitelistNodeList.get(0))
+
+            // recursively call same function on children nodes of nodeToVerify
+            verifyNodeChildren(nodeToVerify, matchingWhitelistNode)
+        }
+        else {
+            throw new DslScriptException(String.format("Your DSL element ${nodeToVerify.name().toString()} is not " +
+                    "listed as a whitelisted element. Whitelisted elements at this " +
+                    "level include - ${whitelistNodeChildrenNames.toString()}"))
+        }
+    }
+
+    static List<String> getNodeChildrenNames(Node node) {
+        List<String> whitelistNodeChildrenNames = []
+        node.children().each {
+            if (it instanceof Node) {
+                whitelistNodeChildrenNames.add(it.name().toString())
+            }
+        }
+        whitelistNodeChildrenNames
+    }
+}
---
+++ job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/VerifyNodeAgainstWhitelistHelperSpec.groovy
@@ -0,0 +1,119 @@
+package javaposse.jobdsl.dsl
+
+import spock.lang.Specification
+
+import java.util.logging.Handler
+import java.util.logging.Level
+import java.util.logging.LogManager
+import java.util.logging.Logger
+
+/**
+ * Testing the whitlelisting feature provided
+ */
+class VerifyNodeAgainstWhitelistHelperSpec extends Specification {
+    static final String LOTS_OF_CHILDREN = '''<?xml version="1.0" encoding="UTF-8"?>
+<project>
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class="hudson.scm.NullSCM"/>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+   <triggers class="vector">
+        <hudson.triggers.SCMTrigger>
+            <spec>H/15 * * * *</spec>
+            <ignorePostCommitHooks>false</ignorePostCommitHooks>
+        </hudson.triggers.SCMTrigger>
+    </triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+      <builder/>
+  </builders>
+  <publishers/>
+  <buildWrappers/>
+</project>
+'''
+
+    static final String EMPTY_PROJECT_XML = '''<?xml version="1.0" encoding="UTF-8"?>
+<project>
+</project>
+'''
+
+    static final String TRIGGER_AT_WRONG_LEVEL = '''<?xml version="1.0" encoding="UTF-8"?>
+<project>
+     <hudson.triggers.SCMTrigger>
+            <spec>H/15 * * * *</spec>
+            <ignorePostCommitHooks>false</ignorePostCommitHooks>
+        </hudson.triggers.SCMTrigger>
+</project>
+'''
+
+    static final String ONLY_TRIGGERS = '''<?xml version="1.0" encoding="UTF-8"?>
+<project>
+     <triggers />
+</project>
+'''
+
+    static final Closure CUSTOM_WORKSPACE_CLOSURE = { Node project ->
+        Node node = methodMissing('customWorkspace', workspacePath)
+        project / node
+    }
+
+    final Node lotsOfChildren = new XmlParser().parse(new StringReader(LOTS_OF_CHILDREN))
+    final Node noProjectChildren = new XmlParser().parse(new StringReader(EMPTY_PROJECT_XML))
+    final Node onlyTriggersChild = new XmlParser().parse(new StringReader(ONLY_TRIGGERS))
+    final Node triggerAtWrongLevel = new XmlParser().parse(new StringReader(TRIGGER_AT_WRONG_LEVEL))
+
+    def setup() {
+        Logger.getLogger('javaposse.jobdsl').setLevel(Level.ALL)
+
+        // Our only choice to allow lower logging is to allow it for everyone since JUL puts the handler in one place
+        // and sets a horrible default
+        LogManager.logManager.getLogger('').handlers.each { Handler handler ->
+            handler.setLevel(Level.ALL)
+        }
+    }
+
+    def 'node valid for whitelist with no children'() {
+        when:
+        VerifyNodeAgainstWhitelistHelper.verifyNode(lotsOfChildren, noProjectChildren)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def 'node not valid if whitelist does not include'() {
+        when:
+        VerifyNodeAgainstWhitelistHelper.verifyNodeChildren(lotsOfChildren, onlyTriggersChild)
+
+        then:
+        thrown(DslScriptException)
+    }
+
+    def 'node valid if whitelist includes'() {
+        when:
+        VerifyNodeAgainstWhitelistHelper.verifyNodeChildren(onlyTriggersChild, lotsOfChildren)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def 'node not valid if whitelist includes but at wrong level'() {
+        when:
+        VerifyNodeAgainstWhitelistHelper.verifyNodeChildren(triggerAtWrongLevel, lotsOfChildren)
+
+        then:
+        thrown(DslScriptException)
+    }
+
+    def 'configure block not valid if whitelist does not include'() {
+        when:
+        VerifyNodeAgainstWhitelistHelper.verifyConfigureBlock(CUSTOM_WORKSPACE_CLOSURE, lotsOfChildren)
+
+        then:
+        thrown(DslScriptException)
+    }
+}
