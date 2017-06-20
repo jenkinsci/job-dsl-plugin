6c76e82a01fb253a679dc94a67c368949721940e
Daniel Spilker <mail@daniel-spilker.com>
2017-06-20 15:02:08 +0200

    enhanced support for the Multijob plugin
    [FIXES JENKINS-44711]

--- job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/MultiJobSpec.groovy
+++ job-dsl-core/src/test/groovy/javaposse/jobdsl/dsl/MultiJobSpec.groovy
@@ -16,4 +16,15 @@ class MultiJobSpec extends Specification {
         then:
         job.node.builders[0].children()[0].name() == 'com.tikal.jenkins.plugins.multijob.MultiJobBuilder'
     }
+
+    def 'call pollSubjobs'() {
+        when:
+        job.pollSubjobs(value)
+
+        then:
+        job.node.pollSubjobs[0].value() == value
+
+        where:
+        value << [true, false]
+    }
 }
