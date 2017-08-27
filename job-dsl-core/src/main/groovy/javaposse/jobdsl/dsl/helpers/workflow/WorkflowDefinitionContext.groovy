9bef89ebb97d6b05b30f2fce824088c1f9e06822
Daniel Spilker <mail@daniel-spilker.com>
2017-08-27 22:37:27 +0200

    enhanced support for the Pipeline plugin
    [FIXES JENKINS-46463]

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/workflow/WorkflowDefinitionContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/workflow/WorkflowDefinitionContext.groovy
@@ -1,21 +1,25 @@
 package javaposse.jobdsl.dsl.helpers.workflow
 
-import javaposse.jobdsl.dsl.AbstractContext
+import javaposse.jobdsl.dsl.AbstractExtensibleContext
 import javaposse.jobdsl.dsl.ContextHelper
+import javaposse.jobdsl.dsl.ContextType
 import javaposse.jobdsl.dsl.DslContext
 import javaposse.jobdsl.dsl.Item
 import javaposse.jobdsl.dsl.JobManagement
 import javaposse.jobdsl.dsl.Preconditions
 import javaposse.jobdsl.dsl.RequiresPlugin
 
-class WorkflowDefinitionContext extends AbstractContext {
-    protected final Item item
-
+@ContextType('org.jenkinsci.plugins.workflow.flow.FlowDefinition')
+class WorkflowDefinitionContext extends AbstractExtensibleContext {
     Node definitionNode
 
     WorkflowDefinitionContext(JobManagement jobManagement, Item item) {
-        super(jobManagement)
-        this.item = item
+        super(jobManagement, item)
+    }
+
+    @Override
+    protected void addExtensionNode(Node node) {
+        definitionNode = ContextHelper.toNamedNode('definition', node)
     }
 
     /**
