0a5a91fc0ddc92867c34a32adef59e4817de3f1d
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2018-02-25 17:10:04 +0000

    JENKINS-49615: Adding workflow-multibranch version annotation for scriptPath

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/workflow/WorkflowBranchProjectFactoryContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/workflow/WorkflowBranchProjectFactoryContext.groovy
@@ -1,13 +1,20 @@
 package javaposse.jobdsl.dsl.helpers.workflow
 
-import javaposse.jobdsl.dsl.Context
+import javaposse.jobdsl.dsl.AbstractContext
+import javaposse.jobdsl.dsl.JobManagement
+import javaposse.jobdsl.dsl.RequiresPlugin
 
-class WorkflowBranchProjectFactoryContext implements Context {
+class WorkflowBranchProjectFactoryContext extends AbstractContext {
     String scriptPath = 'Jenkinsfile'
+
+    WorkflowBranchProjectFactoryContext(JobManagement jobManagement) {
+        super(jobManagement)
+    }
 
     /**
      * Path to use for the workflow branch project pipeline script. Default is `Jenkinsfile`.
      */
+    @RequiresPlugin(id = 'workflow-multibranch', minimumVersion = '2.15')
     void scriptPath(String scriptPath) {
         this.scriptPath = scriptPath
     }
