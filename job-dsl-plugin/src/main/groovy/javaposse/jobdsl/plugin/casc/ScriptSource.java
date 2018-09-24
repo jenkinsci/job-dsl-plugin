0313cfa5d7211e531dc0dc1fcd008ee738949a56
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-09-24 16:51:24 +0200

    (optional) extension to support defining seed jobs on a configuration-as-code managed jenkins master
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/casc/ScriptSource.java
@@ -0,0 +1,14 @@
+package javaposse.jobdsl.plugin.casc;
+
+import hudson.ExtensionPoint;
+import hudson.model.AbstractDescribableImpl;
+
+import java.io.IOException;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public abstract class ScriptSource extends AbstractDescribableImpl<ScriptSource> implements ExtensionPoint {
+
+    public abstract String getScript() throws IOException;
+}
---
+++ job-dsl-plugin/src/test/resources/javaposse/jobdsl/plugin/testjob.groovy
@@ -0,0 +1,11 @@
+job('testJob2') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
