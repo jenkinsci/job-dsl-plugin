b90adee1276825a904c274386e7f88d1d400a210
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2023-03-20 09:02:56 +0100

    Fix tests
    Remove icon examples in doc as it does not work by any means
    We keep a link to custom-folder-icon

--- job-dsl-core/src/main/docs/examples/javaposse/jobdsl/dsl/Folder/icon.groovy
+++ job-dsl-core/src/main/docs/examples/javaposse/jobdsl/dsl/Folder/icon.groovy
@@ -1,28 +1,3 @@
 // use the stock icon
+// see https://github.com/jenkinsci/custom-folder-icon-plugin for custom icons
 folder('stock')
-
-// use https://github.com/jenkinsci/custom-folder-icon-plugin for custom icons
-userContent('customFolderIcons/custom.png', streamFileFromWorkspace('custom.png'))
-folder('custom') {
-    icon {
-        customFolderIcon {
-            foldericon('custom.png')
-        }
-    }
-}
-
-// use https://github.com/jenkinsci/custom-folder-icon-plugin for ionicons
-folder('ionicon') {
-    icon {
-        ioniconFolderIcon {
-            ionicon('jenkins')
-        }
-    }
-}
-
-// use https://github.com/jenkinsci/custom-folder-icon-plugin for build status icon
-folder('build-status') {
-    icon {
-        buildStatusFolderIcon()
-    }
-}
