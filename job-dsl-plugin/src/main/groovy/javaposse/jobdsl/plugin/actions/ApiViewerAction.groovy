1090a3bf99d8da40361043ae5a219268384eeafb
Valentin Delaye <jonesbusy@users.noreply.github.com>
2024-10-30 19:56:42 +0100

    Use monochrome symbol for directory tree (#1477)

--- job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/actions/ApiViewerAction.groovy
+++ job-dsl-plugin/src/main/groovy/javaposse/jobdsl/plugin/actions/ApiViewerAction.groovy
@@ -3,7 +3,7 @@ package javaposse.jobdsl.plugin.actions
 import hudson.model.Action
 
 class ApiViewerAction implements Action {
-    final String iconFileName = '/plugin/job-dsl/images/directory.svg'
+    final String iconFileName = 'symbol-directory plugin-job-dsl'
     final String displayName = 'Job DSL API Reference'
     final String urlName = '/plugin/job-dsl/api-viewer/index.html'
 }
